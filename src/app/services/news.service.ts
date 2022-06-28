b5f71a81a108e0685b4bc151708f667bc218a2c3
UTMIan <UTM20040084@utma.edu.mx>
2022-06-28 08:58:57 -0500

    API update

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,13 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
+  url = environment.urlNewsApi;
   constructor(private http: HttpClient) { }
   getNews(){
-    return this.http.get('https://newsapi.org/v2/everything?q=tesla&from=2022-05-23&sortBy=publishedAt&apiKey=5e1e7d58fef242b4b0354c949518b9f4');
+    const query = {
+      q: "tesla", 
+      from: "2022-05-23",
+      sortBy: "publishedAt",
+      apiKey: "5e1e7d58fef242b4b0354c949518b9f4"
+    }
+    
+    return this.http.get(`${this.url}/v2/everything`,{params: query});
   }
 }
