f437766b2cc9eb5bc2a7d358a0e7ab3e3f98d3d4
UTMIan <UTM20040084@utma.edu.mx>
2022-07-08 00:15:53 -0500

    Prueba 10

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/AngularApp'));
+app.use(express.static('./dist/angular-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/AngularApp/'}),
+    res.sendFile('index.html', {root: 'dist/angular-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3338);
-
-server.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}/`);
-  });
+app.listen(process.env.PORT || 8080);
